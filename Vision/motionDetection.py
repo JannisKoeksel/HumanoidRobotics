1326f328020a62261985f735b7527f77592813b9
JohanRumarKarlquist <rujohan@student.chalmers.se>
2023-09-21 11:23:04 +0200

    Some small changes and comments

--- Vision/motionDetection.py
+++ Vision/motionDetection.py
@@ -7,7 +7,7 @@ import cv2
 from datetime import datetime
 
 # Assigning our static_back to None
-static_back = None
+staticBackground = None
 
 # Capturing video
 video = cv2.VideoCapture(0)
@@ -31,42 +31,44 @@ while True:
 
     # In first iteration we assign the value
     # of static_back to our first frame
-    if static_back is None:
-        static_back = gray
+    if staticBackground is None:
+        staticBackground = gray
         continue
 
     # Difference between static background
     # and current frame(which is GaussianBlur)
-    diff_frame = cv2.absdiff(static_back, gray)
+    diffFrame = cv2.absdiff(staticBackground, gray)
 
     # If change in between static background and
     # current frame is greater than 30 it will show white color(255)
-    thresh_frame = cv2.threshold(diff_frame, 30, 255, cv2.THRESH_BINARY)[1]
-    thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
+    #The motion sensitivity, a greater value is less sensitive
+    motionSensitity = 50
+    threshFrame = cv2.threshold(diffFrame, motionSensitity, 255, cv2.THRESH_BINARY)[1]
+    threshFrame = cv2.dilate(threshFrame, None, iterations=2)
 
     # Finding contour of moving object
-    cnts, _ = cv2.findContours(thresh_frame.copy(),
+    cnts, _ = cv2.findContours(threshFrame.copy(),
                                cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
         if cv2.contourArea(contour) < 10000:
             continue
         motion = 1
-
+        #Add a statement to for transition between states, that is motion is detected -> "show your face" -> face detection
         (x, y, w, h) = cv2.boundingRect(contour)
         # making green rectangle around the moving object
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
 
     # Displaying image in gray_scale
-    cv2.imshow("Gray Frame", gray)
+    #cv2.imshow("Gray Frame", gray)
 
     # Displaying the difference in currentframe to
     # the staticframe(very first_frame)
-    cv2.imshow("Difference Frame", diff_frame)
+    #cv2.imshow("Difference Frame", diff_frame)
 
     # Displaying the black and white image in which if
     # intensity difference greater than 30 it will appear white
-    cv2.imshow("Threshold Frame", thresh_frame)
+    #cv2.imshow("Threshold Frame", thresh_frame)
 
     # Displaying color frame with contour of motion of object
     cv2.imshow("Color Frame", frame)
