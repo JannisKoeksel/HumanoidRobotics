ec4ed9089b3b196dca4d9bf8b27ee5dcb5379c41
emmaesson <emma.esson99@gmail.com>
2023-10-16 15:58:14 +0200

    .

--- main.py
+++ main.py
@@ -1,56 +1,94 @@
+from eventloop import start
 import multiprocessing
-from Vision import face_detection
-from FaceDetection import face_detect as identify_faces
-from FaceDetection.face_recon import process_frame
-import cv2
+from dataclasses import dataclass
 import time
+import cv2
+from states.state import StateMachine, StateData
+from states.behavior import idle
+import states.handler
 
-
-def start():
-    face_queue = multiprocessing.Queue()
-    face_quit = multiprocessing.Value('I', 0)
-    face_p = multiprocessing.Process(target=face_detection, args=(face_queue,face_quit))
-    face_p.start()
+def run(queue):
     
-    face_recon_queue_in = multiprocessing.Queue()
-    face_recon_queue_out = multiprocessing.Queue()
-    face_recon_quit = multiprocessing.Value('I', 0)
-    face_recon_p = multiprocessing.Process(target=identify_faces, args=(face_recon_queue_in,face_recon_queue_out ,face_recon_quit))
-    face_recon_p.start()
+    data = StateData(queue)
+    StateMachine.stateData = data
+    
+    StateMachine.print()
+    
+    StateMachine(idle).run()
+    
+    
 
-    while True:
+
+if __name__ == "__main__":
+    
+    event_loop_queue = multiprocessing.Queue()
+    event_loop_quit = multiprocessing.Value('I', 0)
+    event_loop_p = multiprocessing.Process(target=start, args=(event_loop_queue,event_loop_quit))
+    event_loop_p.start()
+    
+    
+    handler_queue = multiprocessing.Queue(maxsize=1)
+    handler_quit = multiprocessing.Value('I', 0)
+    handler_p = multiprocessing.Process(target=run, args=(handler_queue,))
+    handler_p.start()
+    
+    
+    
+    
+    frame_id = 0
+    while True: 
         time.sleep(0.01)
-        if(face_queue.empty()): 
-            continue
+        if(event_loop_queue.empty()): continue
         
-        latest_data = None
-        
-        while not face_queue.empty():
-            latest_data = face_queue.get(False)
-        
-        
-        # print(latest_data)
-        frames = []
-        for face in latest_data["faces"]:
-            frames.append(latest_data["frame"][face[0]:face[2],face[1]:face[3]])
+        event_data = event_loop_queue.get()
+        while not event_loop_queue.empty():
+            event_data = event_loop_queue.get()
             
-        if(len(frames) > 0):
-            face_recon_queue_in.put({"frames":frames})
         
-        try :
-            print("labels",face_recon_queue_out.get_nowait())
-        except:
-            print("label exception")
         
-        cv2.imshow('frame', latest_data["frame"])
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            cv2.destroyAllWindows()
-            face_recon_quit.value = 1
-            face_quit.value = 1
+        frame = event_data["full_frame"]
+        frame_id += 1
+        # for face in event_data["faces"].faces.values():
+        #     print(face.label)
+            
+        for face in event_data["faces"].faces.values():
+            if(not face.detected ): continue
+            label = face.label
+            if(label == None):
+                label = "..."
+            if(label == -1):
+                label = "Unknown"
+            
+            cv2.putText(frame, label , (face.frame[1], face.frame[0]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+            cv2.rectangle(frame, (face.frame[1],face.frame[0]),(face.frame[3],face.frame[2]), (0,255,0), 3)
+            
+        # print("frame")
+        cv2.imshow("Cam",frame)
+        
+        wait_key = cv2.waitKey(1)
+        if wait_key & wait_key == ord('q'):
+            event_loop_quit.value = 1
+            print("quit")
             break
-
-
-
-
-if __name__ == '__main__':
-    start()
+        
+        
+        if not handler_queue.empty():
+            try:
+                _ = handler_queue.get()
+            except: 
+                pass
+            
+        stateData = {
+            "faces": event_data["faces"].faces,
+            "frame": frame,
+            "frame_id": frame_id
+        }
+        
+        handler_queue.put(stateData)
+        
+    cv2.destroyAllWindows()
+    
+    handler_p.join()
+    event_loop_p.join()
+    
+    exit(0)
