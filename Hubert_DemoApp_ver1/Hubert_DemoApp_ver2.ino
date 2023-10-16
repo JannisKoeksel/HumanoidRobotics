5bcd3af8375936ad9684d92833832daa983df32f
emmaesson <emma.esson99@gmail.com>
2023-10-16 15:08:04 +0200

    hubert

--- Hubert_DemoApp_ver1/Hubert_DemoApp_ver2.ino
+++ Hubert_DemoApp_ver1/Hubert_DemoApp_ver2.ino
@@ -13,15 +13,15 @@ Servo gripper;
 //Init position of all servos
 const int servo_pins[] = {3, 5, 6, 9, 10, 11};
 
-const int pos_init[] = {1700, 1500, 2000, 2127.5, 1485, 1600}; // E: 1650, S: 2200
+const int pos_init[] = {1700, 1500, 2000, 2127.5, 1485, 1390}; // E: 1650, S: 2200, G: 1600
 int curr_pos[6];
 int new_servo_val[6];
-int new_pos[] = {1700, 1500, 2000, 2127.5, 1485, 1600};
+int new_pos[] = {1700, 1500, 2000, 2127.5, 1485, 1390};
 
 const int pos_min[] = {560, 550, 950, 750, 550, 550};
 const int pos_max[] = {2330, 2340, 2400, 2200, 2400, 2150};
 
-const int pos_move[] = {2200, 1000, 1500, 1100, 2300, 1600};
+// const int pos_move[] = {2200, 1000, 1500, 1100, 2300, 1600};
 
 char body_parts[] = {'B','P', 'T', 'S', 'E', 'G'};
 
@@ -46,6 +46,8 @@ void servo_body_ex() {
   delay(10);
 
   curr_pos[0] = now;
+
+  sendServoPosition();
 }
 
 void servo_neck_pan() {
@@ -185,16 +187,6 @@ void setup() {
 	delay(2000);
 }
 
-// void move_to_init() {
-//   // new_pos[6] = pos_init[6];
-//   new_pos[0] = pos_init[0];
-//   new_pos[1] = pos_init[1];
-//   new_pos[2] = pos_init[2];
-//   new_pos[3] = pos_init[3];
-//   new_pos[4] = pos_init[4];
-//   new_pos[5] = pos_init[5];
-// }
-
 void move_one(const char X, const int position) {
   if (X == 'B') { new_pos[0] = position; }
   else if (X == 'P') { new_pos[1] = position; } 
@@ -203,6 +195,12 @@ void move_one(const char X, const int position) {
   else if (X == 'E') { new_pos[4] = position; }
   else if (X == 'G') { new_pos[5] = position; }
 }
+
+void sendServoPosition() {
+  Serial.print(body.read());
+  Serial.println(); // Add a newline character to indicate the end of the data
+}
+
 
 void loop() {
   
--- Hubert_DemoApp_ver1/debugger.py
+++ Hubert_DemoApp_ver1/debugger.py
@@ -2,6 +2,8 @@ import serial
 import time
 import cv2
 
+# import keyboard
+
 import os
 os.system("cls")
 
@@ -12,7 +14,7 @@ ser = serial.Serial('COM3', 57600)
 
 #               body    headPan  headTilt  shoulder  elbow  gripper
 body_parts    = ['B',   'P',    'T',     'S',      'E',   'G']
-position_init = [1700,  1500,    2000,   2127.5,   1485,  1600] # E: 1650, S: 2200
+position_init = [1700,  1500,    2000,   2127.5,   1485,  1390] # E: 1650, S: 2200, G: 1600
 position_min  = [560,   550,     950,    750,      550,   550]
 position_max  = [2330,  2340,    2400,   2200,     2400,  2150]
 
@@ -36,47 +38,54 @@ def correct_position(part, perc_position):
     return new_pos
     
 def shoot():
-    move_servo('S', 1990)
-    move_servo('E', 2217)
-    move_servo('G', 1800)  # change depending on laser pointer
+    # move_servo('S', 1990)
+    # move_servo('E', 2217) # bra med S1852 och E2125
+    # time.sleep(2)
+    # move_servo('G', 1694)  # change depending on laser pointer
+    move_servo('E', 2050)
+    time.sleep(1.5)
+    move_servo('S', 1750)
+    time.sleep(1.5)
+    move_servo('G', 1700)  # 1694
 
-# def move_to_init():
-#     move
+
+def move_to_init():
+    for j, servo in enumerate(body_parts):
+        move_servo(servo, position_init[j])
 
 while True:
 
-    choice = input("Enter 'B', 'P', 'T', 'S', 'E' or 'G' for part, 'shoot', 'idle' or 'close': ")
+    choice = input("Enter 'B', 'P', 'T', 'S', 'E' or 'G' for part, 'shoot', 'scan', 'init' or 'close': ")
 
     if choice == 'close':
-        # move_servo('q', position=1500) # position here is arbitrary
-        # time.sleep(2)
+        move_to_init()
         print('Run is over.')
         break
     elif choice == 'shoot':
-
         shoot()
         print('You are shot!')
         continue
-    elif choice == 'test':
-        move_servo('E', position=1900) # position here is arbitrary
-        move_servo('B', position=1200) # position here is arbitrary
-        move_servo('T', position=1000) # position here is arbitrary
+    elif choice == 'scan':  # this is just to build on for when we can integrate the movement detection
+        for i in range(0,10):  # change this to a while and add a break in each position if movement is detected
+            print(i)
+            if i%4 == 0:
+                move_servo('B', position_max[0]-200)
+                time.sleep(3)
+            elif i%2 == 0:
+                move_servo('B', position_min[0]+200)
+                time.sleep(3)
+            else:
+                move_servo('B', position_init[0])
+                time.sleep(3)
         continue
-    elif choice == 'idle':
-        # for position in range(position_init[0], position_max[0] -100, 50):
-        #     idle_behaviour(position)
-        #     time.sleep(1.5)
-        #     if cv2.waitKey(1) & 0xFF == ord('q'):
-        #         move_servo('s', position=1500) # position here is arbitrary
-        #         time.sleep(2)
-        #         print('You are shot!')
-        #         break
+    elif choice == 'init':
+        move_to_init()
         continue
     elif choice not in body_parts:
         print('Not a valid input. Try again.')
         continue
 
-    position_choice = float(input("Enter rotation from center 0 (-100,100): "))
+    position_choice = int(input("Enter rotation from center 0 (-100,100): "))
     new_position = correct_position(choice, position_choice)
 
     print('Body part chosen: ', choice)
