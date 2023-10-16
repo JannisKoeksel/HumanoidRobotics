dc3c2d4409429b1f3e57a1e20bf2569f7d101026
emmaesson <emma.esson99@gmail.com>
2023-10-16 16:01:39 +0200

    changed main

--- main.py
+++ main.py
@@ -1,94 +1,18 @@
-from eventloop import start
-import multiprocessing
-from dataclasses import dataclass
-import time
+from ultralytics import YOLO
+import serial
 import cv2
-from states.state import StateMachine, StateData
-from states.behavior import idle
-import states.handler
+#Initializes the face detection model
+model = YOLO("Vision\Models\yolov8n-face.pt")
+vid = cv2.VideoCapture(0)
+#Need to do this aswell, because when its done once, the next time takes infinitely faster.
+ret, img = vid.read()
+results = model(img)
+#Initalize servo motors.
 
-def run(queue):
-    
-    data = StateData(queue)
-    StateMachine.stateData = data
-    
-    StateMachine.print()
-    
-    StateMachine(idle).run()
-    
-    
+#Just call another file?
+body_parts    = ['B',   'P',    'T',     'S',      'E',   'G']
+position_init = [1700,  1500,    2000,   2127.5,   1485,  1600] # E: 1650, S: 2200
 
 
-if __name__ == "__main__":
-    
-    event_loop_queue = multiprocessing.Queue()
-    event_loop_quit = multiprocessing.Value('I', 0)
-    event_loop_p = multiprocessing.Process(target=start, args=(event_loop_queue,event_loop_quit))
-    event_loop_p.start()
-    
-    
-    handler_queue = multiprocessing.Queue(maxsize=1)
-    handler_quit = multiprocessing.Value('I', 0)
-    handler_p = multiprocessing.Process(target=run, args=(handler_queue,))
-    handler_p.start()
-    
-    
-    
-    
-    frame_id = 0
-    while True: 
-        time.sleep(0.01)
-        if(event_loop_queue.empty()): continue
-        
-        event_data = event_loop_queue.get()
-        while not event_loop_queue.empty():
-            event_data = event_loop_queue.get()
-            
-        
-        
-        frame = event_data["full_frame"]
-        frame_id += 1
-        # for face in event_data["faces"].faces.values():
-        #     print(face.label)
-            
-        for face in event_data["faces"].faces.values():
-            if(not face.detected ): continue
-            label = face.label
-            if(label == None):
-                label = "..."
-            if(label == -1):
-                label = "Unknown"
-            
-            cv2.putText(frame, label , (face.frame[1], face.frame[0]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-            cv2.rectangle(frame, (face.frame[1],face.frame[0]),(face.frame[3],face.frame[2]), (0,255,0), 3)
-            
-        # print("frame")
-        cv2.imshow("Cam",frame)
-        
-        wait_key = cv2.waitKey(1)
-        if wait_key & wait_key == ord('q'):
-            event_loop_quit.value = 1
-            print("quit")
-            break
-        
-        
-        if not handler_queue.empty():
-            try:
-                _ = handler_queue.get()
-            except: 
-                pass
-            
-        stateData = {
-            "faces": event_data["faces"].faces,
-            "frame": frame,
-            "frame_id": frame_id
-        }
-        
-        handler_queue.put(stateData)
-        
-    cv2.destroyAllWindows()
-    
-    handler_p.join()
-    event_loop_p.join()
-    
-    exit(0)
+#Call to motion detection, 
+
